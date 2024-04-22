b33b362a41b0b0e9b5ab44cf2f8760475f086d2f
YenCheng Lai <yencheng.lai@zyxel.com.tw>
2024-04-22 09:07:22 +0800

    update environment setting commands

--- .cmds/setup.sh
+++ .cmds/setup.sh
@@ -5,8 +5,10 @@ venv_name="fastapi-casbin"
 
 # Check if virtual environment already exists
 if [ -d "$venv_name" ]; then
-    echo "Virtual environment $venv_name already exists."
-    exit 1
+    echo "Deleting existing virtual environment $venv_name"
+    # Deactivate and delete the existing virtual environment
+    # deactivate  # Remove this line
+    rm -rf "$venv_name"  # Delete the existing virtual environment directory
 fi
 
 # Create virtual environment
@@ -18,4 +20,4 @@ source $venv_name/bin/activate
 # Install dependencies from requirements.txt
 pip install -r "$(dirname "$0")/requirements.txt"
 
-echo "Virtual environment $venv_name created and activated."
+echo "Virtual environment $venv_name created."
--- Readme.md
+++ Readme.md
@@ -1,9 +1,17 @@
+# Project Description
+
 This project is trying to build RBAC (Role-Based Access Control) feature with fastAPI and Casbin.
 
-### Build up the environment
-Go to the project folder in command line and run the below command:
-```.sh
-init.sh
-```
+## Build up the environment
 
-It will help you set up the virtual environment and download needed libraries.
+1. Go to the project folder in command line and run the below command:
+
+    ```.sh
+    init.sh
+    ```
+
+2. Activate the venv
+
+    ```.sh
+    source fastapi-casbin/bin/activate
+    ```
