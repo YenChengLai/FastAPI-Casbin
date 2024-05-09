87533842667fc8f94d71a6f56d61104eda2364fd
YenCheng Lai <yencheng.lai@zyxel.com.tw>
2024-05-09 17:10:41 +0800

    update policy and model

--- main.py
+++ main.py
@@ -8,82 +8,85 @@ enforcer = Enforcer("rbac_model.conf", "policy.csv")
 
 
 # Define a dependency to check permission using Casbin
-def check_permission(sub: str, request: Request, path: str):
-    """ trigger casbin validation """
-    if not enforcer.enforce(sub, "update" if request.method == "POST" else "get", path):
+def check_permission(sub: str, request: Request):
+    """trigger casbin validation"""
+    print(enforcer.get_roles_for_user("admin"))
+    if not enforcer.enforce(
+        sub, request.scope["path"][1:], "update" if request.method == "POST" else "get"
+    ):
         raise HTTPException(status_code=403, detail="Permission denied")
 
 
 # Define routes with RBAC authorization
 @app.post("/role-management")
-def role_management(_ = Depends(check_permission)):
-    """ Editing users' roles and permissions """
+def role_management(_=Depends(check_permission)):
+    """Editing users' roles and permissions"""
     return {"message": "role-management accessed!"}
 
 
 @app.post("/remote-device-access")
-def remote_device_access(_ = Depends(check_permission)):
-    """ SSH into remote devices """
+def remote_device_access(_=Depends(check_permission)):
+    """SSH into remote devices"""
     return {"message": "remote-device-access accessed!"}
 
 
 @app.post("/email-communication")
-def email_communication(_ = Depends(check_permission)):
-    """ Sending emails to customers' accounts """
+def email_communication(_=Depends(check_permission)):
+    """Sending emails to customers' accounts"""
     return {"message": "email-communication accessed!"}
 
 
 @app.get("/organization-management")
-def organization_management(_ = Depends(check_permission)):
-    """ Search organization, site, and device's information """
+def organization_management(_=Depends(check_permission)):
+    """Search organization, site, and device's information"""
     return {"message": "organization-management accessed!"}
 
 
 @app.post("/firmware-update")
-def firmware_update(_ = Depends(check_permission)):
-    """ Update remote device's firmware """
+def firmware_update(_=Depends(check_permission)):
+    """Update remote device's firmware"""
     return {"message": "firmware-update accessed!"}
 
 
 @app.get("/firmware-integrity")
-def firmware_integrity(_ = Depends(check_permission)):
-    """ Checking if there's any violation of firmware setting """
+def firmware_integrity(_=Depends(check_permission)):
+    """Checking if there's any violation of firmware setting"""
     return {"message": "firmware-integrity accessed!"}
 
 
 @app.get("/auditing")
-def auditing(_ = Depends(check_permission)):
-    """ Access auditing page to check user behaviors """
+def auditing(_=Depends(check_permission)):
+    """Access auditing page to check user behaviors"""
     return {"message": "auditing accessed!"}
 
 
 @app.post("/announcement")
-def announcement_management(_ = Depends(check_permission)):
-    """ Sending announcement to user accounts """
+def announcement_management(_=Depends(check_permission)):
+    """Sending announcement to user accounts"""
     return {"message": "announcement management accessed!"}
 
 
 @app.post("/approval")
-def approval(_ = Depends(check_permission)):
-    """ Grant pending permissions """
+def approval(_=Depends(check_permission)):
+    """Grant pending permissions"""
     return {"message": "approval pending page accessed!"}
 
 
 @app.post("/org-owner")
-def org_owner_management(_ = Depends(check_permission)):
-    """ Editing organization management """
+def org_owner_management(_=Depends(check_permission)):
+    """Editing organization management"""
     return {"message": "org owner management accessed!"}
 
 
 @app.post("/extend-trial")
-def license_extension(_ = Depends(check_permission)):
-    """ Extend customer's trial """
+def license_extension(_=Depends(check_permission)):
+    """Extend customer's trial"""
     return {"message": "license extension accessed!"}
 
 
 @app.post("/api-key")
-def generate_api_key(_ = Depends(check_permission)):
-    """ Generate OpenAPI key for customers """
+def generate_api_key(_=Depends(check_permission)):
+    """Generate OpenAPI key for customers"""
     return {"message": "generate api key accessed!"}
 
 
