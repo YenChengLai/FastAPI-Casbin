391917ea843b8a5c3b3ff6e5a4cfdf74bfd542fc
YenCheng Lai <yencheng.lai@zyxel.com.tw>
2024-06-03 09:04:48 +0800

    adjust shell initiation commands

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ for script in .cmds/*; do
     # Check if the file is executable
     if [ -x "$script" ]; then
         echo "Running $script"
-        # Run the script
-        "$script"
+        # Source the script to run it in the current shell
+        source "$script"
     fi
 done
